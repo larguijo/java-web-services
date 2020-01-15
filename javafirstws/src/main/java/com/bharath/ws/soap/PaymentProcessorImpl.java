bc3bc92b61bbfca9d2b77070ac968ab550b512b0
Luis Rodriguez <Luis.Rodriguez_Arguijo@telus.com>
2020-01-15 17:42:41 -0600

    Returning a Runtime exception to generate fault tag if credit card is null. Also uploading SOAP UI projects

--- javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessorImpl.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessorImpl.java
@@ -7,6 +7,10 @@ public class PaymentProcessorImpl implements PaymentProcessor {
 
 	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest) {
 		PaymentProcessorResponse paymentProcessorResponse = new PaymentProcessorResponse();
+		if (paymentProcessorRequest.getCreditCardInfo().getCardNumber() == null
+				|| paymentProcessorRequest.getCreditCardInfo().getCardNumber().length() == 0) {
+			throw new RuntimeException("Invalid Card number");
+		}
 		// Business Logic or a call to a Business Logic Class Goes Here.
 		paymentProcessorResponse.setResult(true);
 		return paymentProcessorResponse;
