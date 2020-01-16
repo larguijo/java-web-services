1e59b59cfc8216f6e63a822627dcdaf1fe0ecdef
Luis Rodriguez <Luis.Rodriguez_Arguijo@telus.com>
2020-01-16 15:50:26 -0600

    Generating error messages

--- javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessor.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessor.java
@@ -7,11 +7,12 @@ import javax.jws.WebService;
 
 import com.bharath.ws.soap.dto.PaymentProcessorRequest;
 import com.bharath.ws.soap.dto.PaymentProcessorResponse;
+import com.bharath.ws.soap.exceptions.ServiceException;
 
 @WebService
-public interface PaymentProcessor {
+public interface PaymentProcessor{
 
 	@WebMethod
 	public @WebResult(name = "response") PaymentProcessorResponse processPayment(
-			@WebParam(name="paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest);
+			@WebParam(name="paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest) throws ServiceException;
 }
--- javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessorImpl.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessorImpl.java
@@ -2,14 +2,16 @@ package com.bharath.ws.soap;
 
 import com.bharath.ws.soap.dto.PaymentProcessorRequest;
 import com.bharath.ws.soap.dto.PaymentProcessorResponse;
+import com.bharath.ws.soap.exceptions.ServiceException;
 
 public class PaymentProcessorImpl implements PaymentProcessor {
 
-	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest) {
+	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest)
+			throws ServiceException {
 		PaymentProcessorResponse paymentProcessorResponse = new PaymentProcessorResponse();
 		if (paymentProcessorRequest.getCreditCardInfo().getCardNumber() == null
 				|| paymentProcessorRequest.getCreditCardInfo().getCardNumber().length() == 0) {
-			throw new RuntimeException("Invalid Card number");
+			throw new ServiceException("Invalid Card number");
 		}
 		// Business Logic or a call to a Business Logic Class Goes Here.
 		paymentProcessorResponse.setResult(true);
